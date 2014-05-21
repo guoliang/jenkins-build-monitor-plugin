81ff612aeadd33e8d623f9f8c6cc20da1096718f
Henrik Holmberg <henrik.holmberg89@gmail.com>
2014-05-21 21:57:52 +0200

    Add new option for ordering by status

---
+++ src/main/java/com/smartcodeltd/jenkinsci/plugins/buildmonitor/order/ByStatusAndName.java
@@ -0,0 +1,45 @@
+package com.smartcodeltd.jenkinsci.plugins.buildmonitor.order;
+
+import hudson.model.Result;
+import hudson.model.AbstractProject;
+import hudson.model.Run;
+
+import java.util.Comparator;
+
+public class ByStatusAndName implements Comparator<AbstractProject> {
+
+    @Override
+    public int compare(final AbstractProject a, final AbstractProject b) {
+        final Run lastBuildA = lastCompletedBuild(a);
+        final Run lastBuildB = lastCompletedBuild(b);
+        final int resultCompare = compare(lastBuildA.getResult(), lastBuildB.getResult());
+        if (resultCompare != 0)
+            return resultCompare;
+        return a.getName().compareToIgnoreCase(b.getName());
+    }
+
+    private int compare(final Result resultA, final Result resultB) {
+        if (resultA.isWorseThan(resultB))
+            return -1;
+        if (resultA.isBetterThan(resultB))
+            return 1;
+        return 0;
+    }
+
+    private Run lastCompletedBuild(final AbstractProject a) {
+        final Run lastBuild = a.getLastBuild();
+        if (!lastBuild.isBuilding()) {
+            return lastBuild;
+        }
+        return lastCompletedBuild(lastBuild);
+    }
+
+    private Run lastCompletedBuild(final Run build) {
+        final Run previousBuild = build.getPreviousBuild();
+        if (!previousBuild.isBuilding())
+            return previousBuild;
+        return lastCompletedBuild(previousBuild);
+    }
+
+
+}
